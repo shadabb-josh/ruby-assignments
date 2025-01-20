f89013598748681a71bfe192246b4de58af83f3c
shadabb-josh <shadab.shikalgar@joshsoftware.com>
2025-01-20 15:13:59 +0530

    improved with reverse_each method

--- question_3.rb
+++ question_3.rb
@@ -2,6 +2,14 @@
 # to_a -> creates a array of letters
 # reverse -> as name suggests, it reverses the array
 
+# Previous Code
 for i in ('a'..'z').to_a.reverse
+  puts i
+end
+
+# Improved code
+# ('a'..'z') -> Create a range object
+# reverse_each -> Iterates over the range in reverse  
+('a'..'z').reverse_each do |i|
   puts i
 end
