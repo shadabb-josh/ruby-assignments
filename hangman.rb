5d7917a6e9017a28941ebd9545e33285a2b89489
shadabb-josh <shadab.shikalgar@joshsoftware.com>
2025-01-23 13:45:00 +0530

    fixes

--- hangman.rb
+++ hangman.rb
@@ -4,34 +4,37 @@ chances = 5
 words.each do |word|
   to_guess = "_" * word.length
   chances = 5
-
+  level = 1
+  puts "LEVEL #{level}"
   puts "Guess the word: #{to_guess}"
 
   while chances > 0 && to_guess.include?('_')
     puts "\nEnter a character:"
     c = gets.chomp
 
-    if word.include?(c)
-      word.chars.each_with_index do |char, index|
-        print char, index
-        if char == c
-          to_guess[index] = c
-        end
+    if c.length == 1 and c.length > 0
+      if word.include?(c)
+        index = word.index(c)
+        to_guess[index] = c
+        puts "Correct guess!"
+      else
+        chances -= 1
+        puts "Wrong guess! Chances left: #{chances}"
       end
-      puts "Correct guess!"
+        puts "Current word: #{to_guess}"
     else
-      chances -= 1
-      puts "Wrong guess! Chances left: #{chances}"
+      puts "please enter one character only"
     end
-
-    puts "Current word: #{to_guess}"
+  
   end
 
   if to_guess == word
     puts "Congratulations! You guessed the word: #{word}"
+    level += 1
   else
     puts "Game over! The correct word was: #{word}"
+    level -= 1 if level > 1
   end
-
-  puts "-" * 30  
+  
+  puts "-" * 30 
 end
